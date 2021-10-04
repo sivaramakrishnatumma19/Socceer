63ed788f5d08b61820b89fd3e1fe6868a2c6c48d
sivaramakrishnatumma19 <57492997+sivaramakrishnatumma19@users.noreply.github.com>
2021-10-04 09:31:12 -0400

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -1,6 +1,6 @@
 
-import unittest
-
-class ExampleTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(0, 0)
+class TestStringMethods(unittest.TestCase):
+        def test_goal(self): #testing for goals win
+                 self.assertEqual(df['diff'] = df['Goals'] - df['Goals Allowed'])
+if __name__=='__main__':
+        unittest.main()
