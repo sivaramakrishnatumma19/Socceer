c775cc7d515c6634db05fa876cd456af3833552d
sivaramakrishnatumma19 <57492997+sivaramakrishnatumma19@users.noreply.github.com>
2021-10-04 09:31:29 -0400

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -1,5 +1,5 @@
 
-class TestStringMethods(unittest.TestCase):
+class goalstest(unittest.TestCase):
         def test_goal(self): #testing for goals win
                  self.assertEqual(df['diff'] = df['Goals'] - df['Goals Allowed'])
 if __name__=='__main__':
